d1c8f99737f44b15653d4b037ff3ab7efce32039
sheraj <123598317+sheraj923@users.noreply.github.com>
2024-02-06 00:04:34 -0700

    Update page.js

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -6,8 +6,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
         <h1> CPRG 306 : Web Development 2 - Assignments</h1>
-        <Link href="/week-2" >week-2</Link>
+        <ul>
+          <li><Link href="/week-2" >week-2</Link></li>
+          <li><Link href="/week-3" >week-3</Link></li>
+        </ul>
       </div>
     </main>
   );
 }
+
