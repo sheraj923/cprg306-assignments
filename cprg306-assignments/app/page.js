6ef4afc5b16e3576f26f082e1715dfabe4dfd57c
sheraj <123598317+sheraj923@users.noreply.github.com>
2024-02-12 12:28:03 -0700

    done

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
         <ul>
           <li><Link href="/week-2" >week-2</Link></li>
           <li><Link href="/week-3" >week-3</Link></li>
+          <li><Link href="/week-4" >week-4</Link></li>
         </ul>
       </div>
     </main>
