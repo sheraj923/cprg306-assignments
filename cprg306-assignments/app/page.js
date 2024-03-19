e0a9ff2b7f341298b09ac42bba48dc04c0763124
sheraj <123598317+sheraj923@users.noreply.github.com>
2024-03-18 23:19:08 -0600

    Update page.js

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -1,21 +1,49 @@
 import page from "./week-2/page"
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
-        <h1> CPRG 306 : Web Development 2 - Assignments</h1>
-        <ul>
-          <li><Link href="/week-2" >week-2</Link></li>
-          <li><Link href="/week-3" >week-3</Link></li>
-          <li><Link href="/week-4" >week-4</Link></li>
-          <li><Link href="/week-5" >week-5</Link></li>
-          <li><Link href="/week-6" >week-6</Link></li>
-          <li><Link href="/week-7" >week-7</Link></li>
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-black-500">CPRG 306 : Web Development 2 - Assignments</h1>
+        <ul className="mt-8 space-y-4">
+          <li>
+            <Link href="/week-2" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+              week-2
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-3" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+              week-3
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-4" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+              week-4
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-5" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+             week-5
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-6" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+            week-6
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-7" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+              week-7
+            </Link>
+          </li>
+          <li>
+            <Link href="/week-8" className="flex items-center text-xl font-bold text-black-500 hover:text-blue-500 transition-colors duration-300">
+             week-8
+            </Link>
+          </li>
         </ul>
       </div>
     </main>
-  );
-}
-
+  )}
